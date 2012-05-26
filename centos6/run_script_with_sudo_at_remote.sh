cdd546c18a465cde3b2a448117bae0db826d8731
Hiroaki Nakamura <hnakamur@gmail.com>
2012-05-26 21:43:06 +0900

    Scripts at other directories can be specified.

--- centos6/run_script_with_sudo_at_remote.sh
+++ centos6/run_script_with_sudo_at_remote.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
 if [ $# -lt 2 ]; then
-  echo "Usage: $0 host scriptfile [args...]" 1>&2
+  echo "Usage: $0 host scriptpath [args...]" 1>&2
   exit 1
 fi
 
 host=$1
-scriptfile=$2
+scriptpath=$2
 
 # rest args
 # NOTE: There is a limitation that we cannot have spaces in arguments.
 shift 2
 
-cat $scriptfile | ssh $host "
-file=/tmp/$scriptfile.\$$ &&
-cat > \$file &&
-chmod +x \$file &&
-sudo \$file $@ &&
-rm \$file"
+scriptfile=`basename $scriptpath`
+cat $scriptpath | ssh $host '
+file="/tmp/'"$scriptfile"'.$$" &&
+cat > "$file" &&
+chmod +x "$file" &&
+sudo "$file" $@ &&
+rm "$file"'
