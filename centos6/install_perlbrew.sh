7daa9da0bcc505896434a7ea8d2826a039e8f999
Hiroaki Nakamura <hnakamur@gmail.com>
2012-08-24 12:52:00 +0900

    Set PERLBREW_HOME to /usr/local/perlbrew/etc

--- centos6/install_perlbrew.sh
+++ centos6/install_perlbrew.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 perlbrew_root=/usr/local/perlbrew
+perlbrew_home=/usr/local/perlbrew/etc
 version=5.16.1
 
 export PERLBREW_ROOT=${perlbrew_root} &&
 curl -kL http://install.perlbrew.pl | bash &&
-cat >> /root/.bashrc <<EOF &&
+cat >> /root/.bash_profile <<EOF &&
 
-export PERLBREW_ROOT=${perlbrew_root}
+export PERLBREW_HOME=${perlbrew_home}
 source ${perlbrew_root}/etc/bashrc
 EOF
-. /root/.bashrc &&
+export PERLBREW_HOME=${perlbrew_home} &&
+source ${perlbrew_root}/etc/bashrc &&
 perlbrew init &&
-perlbrew install perl-${version} --as #{version} &&
+perlbrew install perl-${version} &&
 perlbrew switch ${version} &&
 perlbrew install-cpanm &&
 cpanm --self-upgrade &&
 cat <<EOF
 ==============================================================================
-Run "source /root/.bashrc" manually to use perl installed by perlbrew
+Re-login or run "source /root/.bash_profile" manually to enable perlbrew.
 ==============================================================================
 EOF
