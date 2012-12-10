37781da549bfb8de546285b9ba1a00bc8d73d9c9
Hiroaki Nakamura <hnakamur@gmail.com>
2012-12-10 16:08:30 +0900

    Update perl to 5.16.2

--- centos6/install_perlbrew.sh
+++ centos6/install_perlbrew.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 perlbrew_root=/usr/local/perlbrew
 perlbrew_home=/usr/local/perlbrew/etc
-version=5.16.1
+version=5.16.2
 
 export PERLBREW_ROOT=${perlbrew_root} &&
 curl -kL http://install.perlbrew.pl | bash &&
-cat >> /root/.bash_profile <<EOF &&
-
-export PERLBREW_HOME=${perlbrew_home}
-source ${perlbrew_root}/etc/bashrc
-EOF
 export PERLBREW_HOME=${perlbrew_home} &&
 source ${perlbrew_root}/etc/bashrc &&
 perlbrew init &&
@@ -17,6 +12,11 @@ perlbrew install perl-${version} &&
 perlbrew switch ${version} &&
 perlbrew install-cpanm &&
 cpanm --self-upgrade &&
+cat >> /root/.bash_profile <<EOF &&
+
+export PERLBREW_HOME=${perlbrew_home}
+source ${perlbrew_root}/etc/bashrc
+EOF
 cat <<EOF
 ==============================================================================
 Re-login or run "source /root/.bash_profile" manually to enable perlbrew.
